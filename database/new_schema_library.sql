85f257192a517460e1f6506650419663f945e25a
Kofi Frempong <kofifrempong1@gmail.com>
2024-08-30 06:18:13 -0600

    library table added

--- database/new_schema_library.sql
+++ database/new_schema_library.sql
@@ -16,27 +16,26 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `room`
+-- Table structure for table `library`
 --
 
-DROP TABLE IF EXISTS `room`;
+DROP TABLE IF EXISTS `library`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `room` (
+CREATE TABLE `library` (
   `Name` varchar(45) NOT NULL,
-  `Bed` int(11) DEFAULT NULL,
-  `Price` float DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `room`
+-- Dumping data for table `library`
 --
 
-LOCK TABLES `room` WRITE;
-/*!40000 ALTER TABLE `room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+LOCK TABLES `library` WRITE;
+/*!40000 ALTER TABLE `library` DISABLE KEYS */;
+/*!40000 ALTER TABLE `library` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -48,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-30  6:17:17
+-- Dump completed on 2024-08-30  6:17:18
--- database/new_schema_student.sql
+++ database/new_schema_student.sql
@@ -23,13 +23,12 @@ DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
-  `id` int(10) unsigned NOT NULL,
+  `Id` int(10) unsigned NOT NULL,
   `Name` varchar(45) NOT NULL,
   `Subject` varchar(45) NOT NULL,
   `Gpa` float NOT NULL,
-  `Gender` varchar(2) NOT NULL,
-  `roomtaken` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `Gender` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,7 +38,6 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (0,'','',0,'',NULL),(2,'Tom','CS',0,'',NULL),(3,'ric','CD',0,'',NULL);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-29 18:44:02
+-- Dump completed on 2024-08-30  6:17:17
