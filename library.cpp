113b0d3145b965b5ef578e74490f397444e67f25
Kofi <125554531+kofifrempong@users.noreply.github.com>
2024-09-29 18:34:30 -0600

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 const char* HOST = "localhost";
 const char* USER = "root";
-const char* PW = "derpderp"; //your password here 
+const char* PW = ""; //your password here 
 const char* DB = "new_schema";
 
 class Student{
